7bf7f1cbdd9d63c8ea603a308372256ad41344a7
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-03-11 11:31:48 +0100

    Uncaught Error: Class 'MyApp\Socket' not found

--- server/app.php
+++ server/app.php
@@ -1,12 +1,12 @@
 <?php
 
+require dirname( __FILE__ ) . '/vendor/autoload.php';
+require 'config.php';
+
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use MyApp\Socket;
-
-require dirname( __FILE__ ) . '/vendor/autoload.php';
-require 'config.php';
 
 $server = IoServer::factory(
     new HttpServer(
