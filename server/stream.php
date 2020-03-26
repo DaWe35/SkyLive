d8f4d544ce3e597738436aa1a9eaa14ffc253139
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-03-26 13:24:07 +0100

    send 404 heade if stream not found

--- server/stream.php
+++ server/stream.php
@@ -10,7 +10,13 @@ if (empty($streamid) || strlen($streamid) <= 1) {
     exit('Wrong stream id');
 }
 
-$myfile = fopen("streams/" . $streamid. ".txt", "r") or die("Unable to open file!");
+$file = "streams/" . $streamid. ".txt";
+if (file_exists($file)) {
+    $myfile = fopen($file, "r") or die("Unable to open file!");
+} else {
+    http_response_code(404);
+    exit('Stream not found');
+}
 
 ?>
 #EXTM3U
@@ -19,7 +25,7 @@ $myfile = fopen("streams/" . $streamid. ".txt", "r") or die("Unable to open file
 #EXT-X-MEDIA-SEQUENCE:0
 
 <?php
-$content = fread($myfile,filesize("streams/" . $streamid. ".txt"));
+$content = fread($myfile,filesize($file));
 if (isset($_GET['portal']) && !empty($_GET['portal'])) {
     
     $content = str_replace("https://siasky.net",$_GET['portal'],$content);
