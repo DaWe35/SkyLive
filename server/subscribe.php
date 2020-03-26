7b5ae26a795c1722ce6631af46bfe25a98553dd6
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-03-26 13:02:02 +0100

    subscribe print if there is any error

--- server/subscribe.php
+++ server/subscribe.php
@@ -1,17 +1,21 @@
 <?php
-$file = "mail_list.php";
+$file = "subscriptions/mail_list.php";
 if (!isset($_POST['email']) || empty($_POST['email'])) {
     exit('Email is empty');
 }
 
 if (!file_exists($file)) {
-    touch($file);
-    file_put_contents($file,"<?php\n", FILE_APPEND);
+    if (!touch($file)) {
+        exit('Error: cannot touch the file');
+    }
+    file_put_contents($file,"<?php /*\n", FILE_APPEND);
 }
 
 $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 if (empty($email)) {
     exit('Wrong email format');
 }
-file_put_contents($file, $email . "\n", FILE_APPEND);
+if (!file_put_contents($file, $email . "\n", FILE_APPEND)) {
+    exit('Sorry, there is an unexpected error');
+}
 header('Location: index.php?subscribed');
