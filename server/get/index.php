947f1d74fb40922e1262676ab2bc1b1145e367a0
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-05-18 20:53:06 +0200

    order changing

--- server/get/index.php
+++ server/get/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$streams_stmt = $db->prepare("SELECT `streamid`, `title`, `description`, `scheule_time`, `started`, `finished` FROM `stream` WHERE visibility = 'public' ORDER BY streamid DESC LIMIT 100");
+$streams_stmt = $db->prepare("SELECT `streamid`, `title`, `description`, `scheule_time`, `started`, `finished` FROM `stream` WHERE visibility = 'public' ORDER BY scheule_time DESC, streamid DESC  LIMIT 100");
 if (!$streams_stmt->execute()) {
     exit('Database error');
 }
