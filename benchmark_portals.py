0e1b97feb622a60cfbfdef18117c4f13fe5aab4a
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-05-28 14:50:37 +0200

    better benchmark

--- benchmark_portals.py
+++ benchmark_portals.py
@@ -1,6 +1,19 @@
 import time
 from siaskynet import Skynet
 import os
+import eventlet
+eventlet.monkey_patch()
+
+def time_to_str(value):
+    if value == 20:
+        time = 'timeout'
+    elif value == 999:
+        time = 'error'
+    else:
+        time = value
+    return time
+
+print('Starting benchmark')
 
 f = open("2MBfile.txt", "w+")
 text = ''
@@ -11,6 +24,7 @@ f.write(text)
 f.close()
 
 portals = [
+    'https://skynet.coolhd.hu',
     'https://skynet.luxor.tech',
     'https://vault.lightspeedhosting.com',
     'https://skydrain.net',
@@ -21,15 +35,9 @@ portals = [
     'https://skyportal.xyz',
     'https://skynet.cloudloop.io',
     'https://siacdn.com',
-    '',
-    'https://siasky.net',
-    'https://siasky.net',
-    'https://siasky.net',
-    'https://siasky.net',
-    'https://siasky.net',
-    'https://siasky.net',
     'https://siasky.net',
 ]
+results = []
 
 for portal in portals:
     start_time = time.time()
@@ -41,11 +49,23 @@ for portal in portals:
         'custom_filename': ''
     })
     try:
-        skylink = Skynet.upload_file('2MBfile.txt', opts)
-        uploadtime = round(time.time() - start_time, 2)
-        print(uploadtime, portal)
+        try:
+            with eventlet.Timeout(20):
+                skylink = Skynet.upload_file('2MBfile.txt', opts)
+            uploadtime = round(time.time() - start_time, 2)
+            current_result = [uploadtime, portal]
+            
+        except eventlet.timeout.Timeout:
+            current_result = [20, portal]
     except Exception as e:
-        print('error', portal)
+        current_result = [999, portal]
+    results.append(current_result)
+    print('Benchmarking', str(len(results)) + '/' + str(len(portals)), 'portal. Current:', time_to_str(current_result[0]), current_result[1])
 
+print('\nRESULTS:\n')
+results.sort(key=lambda x: x[0])
+for elem in results:
+    time = time_to_str(elem[0])
+    print(time, elem[1])
 
 os.remove("2MBfile.txt")
